969b7899a2c96b6b9be126fea702eb726ddcfddd
mahta-zare <73464595+mahta-zare@users.noreply.github.com>
2025-11-18 16:14:06 -0600

    Disable build functions in script.groovy
    Comment out buildJar and buildImage functions.

--- script.groovy
+++ script.groovy
@@ -1,17 +1,17 @@
-def buildJar() {
-    echo "building application..."
-    sh 'mvn package'
+// def buildJar() {
+//     echo "building application..."
+//     sh 'mvn package'
 
-}
+// }
 
-def buildImage() {
-    echo "building image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t mahtazare/test_repo:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mahtazare/test_repo:jma-2.0'
-    }
-}
+// def buildImage() {
+//     echo "building image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+//         sh 'docker build -t mahtazare/test_repo:jma-2.0 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push mahtazare/test_repo:jma-2.0'
+//     }
+// }
 
 def deployJar() {
     echo "deploying application..."
